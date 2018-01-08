ed835fd84e75f3db36140d7f64b9b8c6ac3f7eb3
Kevin Pak <kedpak@yahoo.com>
2018-01-08 14:32:35 -0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# notes_application
+# Notes application
+
+Application to take notes utilizing node.js
---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('begin app');
+
+const fs = require('fs');
+const os = require('os');
+
+let user = os.userInfo();
+console.log(user);
+fs.appendFile('greeting.txt', `Hello ${user.username}!`, function(err) {
+        if (err) {
+            console.log('cannot write file');
+        }
+});
