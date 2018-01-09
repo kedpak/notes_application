55bb1148337a75ddab00366eab1d24c05fe9b4af
Kevin Pak <kedpak@yahoo.com>
2018-01-09 13:29:29 -0800

    add addNote function property to exports

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const notes = require('./notes.js');
 
 let user = os.userInfo();
 
+let result = notes.addNote;
+console.log(result());
+
+/*
 fs.appendFile('greeting.txt', `Hello ${user.username}! You are ${notes.age} years old!`, function(err) {
         if (err) {
             console.log('cannot write file');
         }
 });
+*/
