122c35d79b0ff62ba2a071e8749548fe022ced04
Kevin Pak <kedpak@yahoo.com>
2018-01-09 13:15:34 -0800

    add age propery to moudle exports object and print new property into text file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const notes = require('./notes.js');
 
 let user = os.userInfo();
 
-fs.appendFile('greeting.txt', `Hello ${user.username}!`, function(err) {
+fs.appendFile('greeting.txt', `Hello ${user.username}! You are ${notes.age} years old!`, function(err) {
         if (err) {
             console.log('cannot write file');
         }
