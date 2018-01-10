139783c06012befc1e407e42e6494ba6405f191d
Kevin Pak <kedpak@yahoo.com>
2018-01-09 17:57:49 -0800

    set up command line options

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ console.log('starting app.js');
 const fs = require('fs');
 const _ = require('lodash');
 const notes = require('./notes.js');
+
+const command = process.argv[2];
+console.log('Commmand: ', command);
+
+if (command === 'add') {
+  console.log('add the stuff');
+} else if (command === 'list'){
+  console.log('this is list');
+} else if (command === 'read') {
+  console.log('get note back');
+} else if (command === 'remove') {
+  console.log('removed note');
+}
+else {
+  console.log('Command does not exist');
+}
